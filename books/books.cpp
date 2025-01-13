0898f6881ccc343637c1bd6f4118f0d9e2cebb45
Danofum <ter44daniil@gmail.com>
2025-01-14 02:30:31 +0300

    Друг

--- books/books.cpp
+++ books/books.cpp
@@ -2,173 +2,124 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <Windows.h>
 
 using namespace std;
 
-struct Book {
+struct book {
     string name;
     string author;
-    string publishing;
+    string publish;
     string year;
-    string cost;
+    string price;
 };
 
-int getInputInt(int min, int max) {
-    setlocale(LC_ALL, "RU");
-    int input;
-    while (true) {
-        cin >> input;
-        if (cin.fail() || input < min || input > max) {
-            cin.clear();
-            cin.ignore(1000, '\n');
-            cout << "Ошибка. Введите число от " << min << " до " << max << ": ";
-        } else {
-            return input;
-        }
+int intCheck(int integer, int max) {
+    while (cin.fail() || integer < 0 || integer > max) {
+        cin.clear();
+        cin.ignore(1000, '\n');
+        cout << "Ошибка, вы должны ввести целое неотрицательное число." << endl << "Введите число ещё раз: " << endl;
+        cin >> integer;
     }
+    return integer;
 }
 
 int main() {
-    setlocale(LC_ALL, "RU");
-    string fileLocation = "E:/books.txt";  // Путь к файлу
-    vector<Book> books;
-    ifstream inFile(fileLocation);
-    
-    if (!inFile.is_open()) {
-        cout << "Не удалось открыть файл!" << endl;
-        return 1;
-    }
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-    // Загрузка данных из файла
-    Book book;
-    while (inFile >> book.name >> book.author >> book.publishing >> book.year >> book.cost) {
-        books.push_back(book);
-    }
-    inFile.close();
-
+    vector<book> books;
+    string fileLocation = "E:\\books.txt";
+    ifstream in(fileLocation);
     int choice;
+
+    if (in.is_open()) {
+        book tempBook;
+        while (getline(in, tempBook.name, ';') && getline(in, tempBook.author, ';') && getline(in, tempBook.publish, ';') && getline(in, tempBook.year, ';') && getline(in, tempBook.price)) {
+
+            books.push_back(tempBook);
+        }
+        in.close();
+    }
+
     do {
-        cout << "\nМеню:\n";
-        cout << "1. Показать книги\n";
-        cout << "2. Добавить новую книгу\n";
-        cout << "3. Удалить книгу\n";
-        cout << "4. Добавить книги из другого файла\n";
-        cout << "5. Фильтрация по цене\n";
-        cout << "6. Выход\n";
-        cout << "Выберите действие: ";
+        cout << "Меню:" << endl;
+        cout << "1. Показать книги" << endl;
+        cout << "2. Добавить новую книгу" << endl;
+        cout << "3. Отсортировать по цене (от min до max)" << endl;
+        cout << "4. Выход" << endl;
+
         cin >> choice;
+        choice = intCheck(choice, 4);
 
         switch (choice) {
-            case 1: {
-                cout << "Название \t Автор \t Издательство \t Год \t Цена" << endl;
-                for (const auto& book : books) {
-                    cout << book.name << " \t " << book.author << " \t " << book.publishing << " \t " << book.year << " \t " << book.cost << endl;
-                }
-                break;
+        case 1: {
+            cout << "Книга \t \t Автор \t \t Издательство \t Год \t Стоимость" << endl;
+            for (int i = 0; i < books.size(); i++) {
+                cout << books[i].name << "\t \t " << books[i].author << "\t \t " << books[i].publish << "\t " << books[i].year << "\t " << books[i].price << endl;
             }
-            case 2: {
-                Book newBook;
-                cout << "Введите название книги: ";
-                cin.ignore();
-                getline(cin, newBook.name);
-                cout << "Введите имя автора: ";
-                getline(cin, newBook.author);
-                cout << "Введите издательство: ";
-                getline(cin, newBook.publishing);
-                cout << "Введите год издания: ";
-                getline(cin, newBook.year);
-                cout << "Введите цену книги: ";
-                getline(cin, newBook.cost);
-
-                books.push_back(newBook);
-                
-                // Добавляем книгу в файл
-                ofstream outFile(fileLocation, ios::app);
-                if (!outFile.is_open()) {
-                    cout << "Не удалось открыть файл для добавления!" << endl;
-                    return 1;
-                }
-                outFile << newBook.name << " " << newBook.author << " " << newBook.publishing << " " << newBook.year << " " << newBook.cost << endl;
-                outFile.close();
-                break;
-            }
-            case 3: {
-                int index;
-                cout << "Введите номер книги для удаления (от 0 до " << books.size() - 1 << "): ";
-                cin >> index;
-
-                if (index >= 0 && index < books.size()) {
-                    books.erase(books.begin() + index);
-                    cout << "Книга удалена!" << endl;
-
-                    // Перезаписываем файл с новыми данными
-                    ofstream outFile(fileLocation);
-                    if (!outFile.is_open()) {
-                        cout << "Не удалось открыть файл для перезаписи!" << endl;
-                        return 1;
-                    }
-                    for (const auto& book : books) {
-                        outFile << book.name << " " << book.author << " " << book.publishing << " " << book.year << " " << book.cost << endl;
-                    }
-                    outFile.close();
-                } else {
-                    cout << "Некорректный индекс!" << endl;
-                }
-                break;
-            }
-            case 4: {
-                string anotherFile;
-                cout << "Введите путь к файлу для добавления данных: ";
-                cin >> anotherFile;
-
-                ifstream inFile(anotherFile);
-                if (!inFile.is_open()) {
-                    cout << "Не удалось открыть файл для добавления!" << endl;
-                    break;
-                }
-
-                while (inFile >> book.name >> book.author >> book.publishing >> book.year >> book.cost) {
-                    books.push_back(book);
-                }
-                inFile.close();
-
-                // Перезаписываем файл с новыми данными
-                ofstream outFile(fileLocation);
-                if (!outFile.is_open()) {
-                    cout << "Не удалось открыть файл для перезаписи!" << endl;
-                    return 1;
-                }
-                for (const auto& book : books) {
-                    outFile << book.name << " " << book.author << " " << book.publishing << " " << book.year << " " << book.cost << endl;
-                }
-                outFile.close();
-                break;
-            }
-            case 5: {
-                double minPrice, maxPrice;
-                cout << "Введите минимальную цену: ";
-                cin >> minPrice;
-                cout << "Введите максимальную цену: ";
-                cin >> maxPrice;
-
-                cout << "Книги в пределах диапазона цен (" << minPrice << " - " << maxPrice << "):" << endl;
-                cout << "Название \t Автор \t Издательство \t Год \t Цена" << endl;
-                for (const auto& book : books) {
-                    double cost = stod(book.cost);  // Преобразуем строку в число
-                    if (cost >= minPrice && cost <= maxPrice) {
-                        cout << book.name << " \t " << book.author << " \t " << book.publishing << " \t " << book.year << " \t " << book.cost << endl;
-                    }
-                }
-                break;
-            }
-            case 6:
-                cout << "Выход из программы." << endl;
-                break;
-            default:
-                cout << "Неверный выбор. Попробуйте снова." << endl;
-                break;
+            break;
         }
-    } while (choice != 6);
+        case 2: {
+            book addBook;
+            cout << "Введите название книги: " << endl;
+            cin >> addBook.name;
+            cout << "Введите имя автора: " << endl;
+            cin >> addBook.author;
+            cout << "Введите название издательства: " << endl;
+            cin >> addBook.publish;
+            cout << "Введите год: " << endl;
+            cin >> addBook.year;
+            cout << "Введите стоимость: " << endl;
+            cin >> addBook.price;
+
+            books.push_back(addBook);
+
+            ofstream out(fileLocation, ios::app); 
+            if (out.is_open()) {
+                out << addBook.name << ";" << addBook.author << ";" << addBook.publish << ";" << addBook.year << ";" << addBook.price << endl;
+                out.close();
+                cout << "Книга добавлена!" << endl;
+            }
+            else {
+                cout << "Ошибка открытия файла!" << endl;
+            }
+            break;
+        }
+        case 3: {
+            if (books.empty()) {
+                cout << "Список книг пуст!" << endl;
+            }
+            else {
+                string minPrice = books[0].price;
+                string maxPrice = books[0].price;
+
+                for (int i = 1; i < books.size(); i++) {
+                    if (books[i].price < minPrice) {
+                        minPrice = books[i].price;
+                    }
+                    if (books[i].price > maxPrice) {
+                        maxPrice = books[i].price;
+                    }
+                }
+
+                cout << "Минимальная цена: " << minPrice << endl;
+                cout << "Максимальная цена: " << maxPrice << endl;
+            }
+            break;
+        }
+        case 4: {
+            cout << "Выход из программы." << endl;
+            break;
+        }
+        default: {
+            cout << "Неверный выбор!" << endl;
+            break;
+        }
+        }
+        
+
+    } while (choice != 4);
 
     return 0;
 }
